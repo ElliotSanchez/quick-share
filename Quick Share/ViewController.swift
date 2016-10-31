0c6801f87d6383913aaa9053aed51ec2969d95b8
hostname <hostname@localhost.local>
2016-10-31 13:40:45 -0500

    added UITableViewDelegate inheritance to root ViewController, and added a new view controller to be called upon click. improved commenting to show which functions in root ViewController where related to which table functions.

--- Quick Share/ViewController.swift
+++ Quick Share/ViewController.swift
@@ -8,23 +8,29 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDataSource {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet weak var tableView: UITableView!
     
     let reuseIdentifier = "tableViewCell"
     
-    // dummy array to check and make sure table view is working
-    var dummyObjects = ["hi", "hello", "there", "I'm", "awesome!"]
+    // dummy array to check and make sure table view is working. 
+    // var dummyObjects = ["hi", "hello", "there", "I'm", "awesome!"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        // Set the data source for our tableView as this viewController class, aka, self
+        // Set the data source and delegated responder for our tableView as this viewController class, aka, self
         tableView.dataSource = self
+        tableView.delegate = self
     }
 
+    
+    //////////////////////////////////////////////
+    // UITableViewDataSource REQUIRED FUNCTIONS //
+    //////////////////////////////////////////////
+    // Returns cell for display using custom MyTableViewCell class
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell: MyTableViewCell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath) as! MyTableViewCell
         
@@ -33,14 +39,24 @@ class ViewController: UIViewController, UITableViewDataSource {
         return cell
     }
     
-    
     func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
     
+    // Currently sets length of table at fixed length
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return dummyObjects.count
+        return 10
     }
+    
+    ///////////////////////////////////
+    // UITableViewDelegate FUNCTIONS //
+    ///////////////////////////////////
+    //
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+    }
+    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
