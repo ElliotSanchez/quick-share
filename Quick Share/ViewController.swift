7dcab22050d2ddf42e068798381815b029c2233b
hostname <github@elliotsanchez.com>
2016-10-31 12:29:32 -0500

    added navigation controller and table view with outlet

--- Quick Share/ViewController.swift
+++ Quick Share/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    let reuseIdentifier = "tableViewCell"
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
