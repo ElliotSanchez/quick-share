affe174fc48c81d24efd7e881a03d87a33fd30ef
hostname <github@elliotsanchez.com>
2016-11-02 09:24:45 -0500

    functional sharing across Facebook and twitter, other services not functional

--- Quick Share/ShowImageViewController.swift
+++ Quick Share/ShowImageViewController.swift
@@ -8,12 +8,37 @@
 
 import UIKit
 import Photos
+import Social
 
-class ShowImageViewController: UIViewController {
+class ShowImageViewController: UIViewController, UIDocumentInteractionControllerDelegate {
 
     @IBOutlet weak var imageView: UIImageView!
     
     var asset: PHAsset?
+    
+    @IBAction func shareButtonClicked (_ sender: UIButton) {
+        switch sender.tag {
+        case 1:
+            if let vc = SLComposeViewController(forServiceType: SLServiceTypeFacebook) {
+                shareFacebookTwitter(vc: vc)
+            }
+        case 2: print("instagram")
+        case 3: print("fb messenger")
+        case 4:
+            if let vc = SLComposeViewController(forServiceType: SLServiceTypeTwitter) {
+                shareFacebookTwitter(vc: vc)
+            }
+        case 5: print("pinterest")
+        case 6: print("whatsapp")
+        default: print("nothing")
+        }
+    }
+    
+    func shareFacebookTwitter (vc: SLComposeViewController) {
+        vc.setInitialText("Check out this picture I shared with Quick Share!")
+        vc.add(imageView.image)
+        present(vc, animated: true, completion: nil)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
